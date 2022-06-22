9c5a7be91f0cb6b064a0af8e6e9b701d4768d0f0
asasases <2284916844@qq.com>
2022-06-22 20:21:25 +0800

    Create HelloWorld.java
    one try

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class HelloWorld{
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int i=input.nextInt();
+		System.out.println(i);
+	}
+}
